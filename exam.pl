339e9d94867a6a574a71cb1d321e0710cc26a99c
SVladkov <s.v.vladkov@gmail.com>
2015-09-04 00:51:27 +0300

    And the task

--- exam.pl
+++ exam.pl
@@ -1,3 +1,7 @@
+% L=[a1, a2, ..., an] represents a the digits of the decimal number a1a2...an. For example L=[1, 2, 3, 4] represents 1234.
+% Without using prolog's arithmetic operations for numbers larger than 100, write a predicate that in the list Y stores the 
+% representation of L multiplied by 7.
+
 % q(X, Y) :- myReverse(X, R) p-help(R, Y, 0).
 
 myReverse([], []).
