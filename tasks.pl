d59cbcc39ba0bfcb5fcd0c1c2543e23d52e38231
SVladkov <s.v.vladkov@gmail.com>
2016-04-14 17:23:10 +0300

    Wrote quickSort, sublist generation, subset generation, permutation and some other tasks

--- tasks.pl
+++ tasks.pl
@@ -44,7 +44,7 @@ genL(X) :- append([1, 2| X], [6, 7], [1, 2, 3, 4, 5, 6, 7]).
 % X is list of lists of numbers. Write a predicate that tells if two of the elements of some element of X are equal to 5
 twoSubelementsAreEqualToFive(X) :- member(L, X), append(_, [A|Q], L), member(B, Q), A=5, B=5.
 
-% X is list of lists of numbers. Write a predicate that tells if every two elements of X have at least 3 different common elements
+% X is list of lists of numbers. Write a predicate that tells if each two elements of X have at least 3 different common elements
 eachTwoElementsThreeCommon(L) :- not((append(_, [X|Q], L), member(Y, Q),
 									not((
 										member(A, X), member(A, Y),
@@ -53,3 +53,68 @@ eachTwoElementsThreeCommon(L) :- not((append(_, [X|Q], L), member(Y, Q),
 										not(A=B), not(A=C), not(B=C)
 									))
 								 )).
+								 
+% X is list of lists of numbers. Write a predicate that tells if each two elements of X have common element that is not part of some other element of X
+eachTwoElementsTwoUnique(L) :- not((member(A, L), member(B, L),
+									not((member(X, A), member(X, B), member(C, L),
+										not(member(X, C))
+									))
+							   )).
+							   
+% take two different elements of L
+takeTwo(L, A, B) :- append(_, [A|Q], L), member(B, Q).
+
+% get continious sublist of a list
+sublist(L, R) :- append(_, X, L), append( R, _, X).
+
+% get two consecutive elements of L
+consecutive(L, A, B) :- append(_, [A, B|_], L).
+
+isSorted(L) :- not((consecutive(L, A, B), A>B)).
+
+% take one element out of the list
+out(L, E, R) :- append(A, [E|B], L), append(A, B, R).
+
+perm(L, [E|Q]) :- out(L, E, R), perm(R, Q).
+perm([], []).
+
+subsetGen(L, [E|R]) :- out(L, E, Q), subsetGen(Q, R).
+subsetGen(L, R) :- out(L, E, Q), subsetGen(Q, R).
+subsetGen([],[]).
+
+% Write quick sort
+% !!! =<
+split([Head|List], Element, [Head|Leftpart], Rightpart) :- Head =< Element,
+	split(List, Element, Leftpart, Rightpart).
+	
+split([Head|List], Element, Leftpart, [Head|Rightpart]) :- Head > Element,
+	split(List, Element, Leftpart, Rightpart).
+	
+split([], S, [], []).
+
+quickSort([E|L], M) :-	
+	%member(E, L),
+	split(L, E, L1, L2),
+	quickSort(L1, M1), quickSort(L2, M2),
+	append(M1, [E|M2], M).
+	
+quickSort([],[]).
+
+sum([H|L], S) :- sum(L, S1), S is S1+H.
+sum([], 0).
+
+% You are given lists X and Y. Generate in R all sublists of Y, such that the length of R is the same as the length of X 
+% and each element of R is bigger than the corresponding element of X and the last elements of Z and Y are equal
+
+myLast(L, E) :- append(_, [E], L).
+
+myLength([H|L], R) :- myLength(L, R1), R is R1+1.
+myLength([], 0). 
+
+generateSublists(X, Y, P) :- sublist(Y, P), 
+							myLength(P, Pl), myLength(X, Xl), Pl =:= Xl,
+							myLast(P, Plast), myLast(X, Xlast), Plast =:= Xlast,
+							corresponding(X, P).
+							
+corresponding([A|X], [B|P]) :- A=<B, corresponding(X, P).
+corresponding([], []).
